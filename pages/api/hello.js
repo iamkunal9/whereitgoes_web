1d59729555a1c8811aa6e2b91411412cb6b76a97
iamkunal9 <ks4428560@gmail.com>
2023-08-05 17:34:21 +0530

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+var Whereitgoes = require("whereitgoes");
+var whereitgoes = new Whereitgoes();
+export default async function handler(req, res) {
+  const { url } = req.query; // Use req.query to get query parameters from the URL
+  console.log(url)
+  if(url==undefined){
+    return res.status(500).json({ error: "Nops" });
+  }
+  try {
+    console.log(url)
+    var json = await whereitgoes.getredirect(url);
+    res.status(200).json({ json }); // Sending the final URL in the response
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 }
